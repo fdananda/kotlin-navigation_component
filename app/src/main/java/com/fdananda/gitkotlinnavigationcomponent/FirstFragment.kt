9f181d84344e57370d8fb6d1b60751f71f7cddc9
fdananda <fdananda@hotmail.com>
2021-09-10 15:27:00 -0300

    First commit about Navigation Component

---
+++ app/src/main/java/com/fdananda/gitkotlinnavigationcomponent/FirstFragment.kt
@@ -0,0 +1,27 @@
+package com.fdananda.gitkotlinnavigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.view.*
+
+class FirstFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        view.textView1.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.navigation_to_secondFragment)}
+
+        return view
+    }
+}
---
+++ app/src/main/java/com/fdananda/gitkotlinnavigationcomponent/SecondFragment.kt
@@ -0,0 +1,27 @@
+package com.fdananda.gitkotlinnavigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_second.view.*
+
+class SecondFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+
+        view.textView2.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.navigation_to_firstFragment)}
+
+        return view
+    }
+}
